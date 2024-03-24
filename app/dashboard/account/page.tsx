bce892652f85a64b91252771db7a737f80ff1559
pscetx <pscetxvn@gmail.com>
2024-03-24 16:24:52 +0700

    ok

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,4 +1,6 @@
 import { lusitana } from "@/app/ui/fonts";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import { signOut } from "@/auth";
 
 export default async function Page() {
   return (
@@ -6,6 +8,17 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Tài khoản
       </h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <PowerIcon className="w-6" />
+          <div className="hidden md:block">Sign Out</div>
+        </button>
+      </form>
     </main>
   );
 }
